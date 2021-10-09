707990e8a74813d5a4f83a8241205b7e658e1bc7
Vikas <47058321+ipvikas@users.noreply.github.com>
2021-10-09 22:13:13 +0530

    Update Txt_To_Audio_SPEECH.py

--- Txt_To_Audio_SPEECH.py
+++ Txt_To_Audio_SPEECH.py
@@ -30,7 +30,7 @@ if st.button('Text to SPEECH'):#for streamlit
         # save the audio file
         t1.save("welcome.mp3")
         from IPython.display import Audio
-        st.wrtite(Audio('welcome.mp3'))
+        
         Audio('welcome.mp3')
       
 else: pass
