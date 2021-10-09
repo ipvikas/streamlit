fe519d03758c1e92600825e2713af680f9e0b9be
Vikas <47058321+ipvikas@users.noreply.github.com>
2021-10-09 09:17:49 +0530

    Update AIReviews.py

--- AIReviews.py
+++ AIReviews.py
@@ -7,7 +7,7 @@
 #! pip install transformers
 #! pip install SentencePiece
 
-import torch
+#import torch
 from transformers import PegasusForConditionalGeneration, PegasusTokenizer
 import streamlit as st
 
@@ -15,9 +15,10 @@ import streamlit as st
 
 def download_model():
     model_name = 'tuner007/pegasus_paraphrase'
-    torch_device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    #torch_device = 'cuda' if torch.cuda.is_available() else 'cpu'
     tokenizer = PegasusTokenizer.from_pretrained(model_name)
-    model = PegasusForConditionalGeneration.from_pretrained(model_name).to(torch_device)
+    #model = PegasusForConditionalGeneration.from_pretrained(model_name).to(torch_device)
+    model = PegasusForConditionalGeneration.from_pretrained(model_name)
     return model, tokenizer
     
 st.title('AI Reviews')
@@ -29,7 +30,8 @@ if st.button('Click to get AI Reviews'):
         st.write('Please enter text to get AI Reviews')
     else:   
         model_name = 'tuner007/pegasus_paraphrase'        
-        batch = tokenizer.prepare_seq2seq_batch(tgt_text,truncation=True,padding='longest',max_length=60, return_tensors="pt").to(torch_device)
+        #batch = tokenizer.prepare_seq2seq_batch(tgt_text,truncation=True,padding='longest',max_length=60, return_tensors="pt").to(torch_device)
+        batch = tokenizer.prepare_seq2seq_batch(tgt_text,truncation=True,padding='longest',max_length=60, return_tensors="pt")
         translated = model.generate(**batch,max_length=60,num_beams=10, num_return_sequences=num_return_sequences, temperature=1.5)
         tgt_text = tokenizer.batch_decode(translated, skip_special_tokens=True)
         #return tgt_text        
