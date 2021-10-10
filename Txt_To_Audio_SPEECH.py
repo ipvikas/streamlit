b6b6629c6955091c6597362b65426618d9fc673b
Vikas <47058321+ipvikas@users.noreply.github.com>
2021-10-10 14:48:19 +0530

    Update Txt_To_Audio_SPEECH.py
    Added line to play audio file

--- Txt_To_Audio_SPEECH.py
+++ Txt_To_Audio_SPEECH.py
@@ -30,7 +30,11 @@ if st.button('Text to SPEECH'):#for streamlit
         # save the audio file
         t1.save("welcome.mp3")
         from IPython.display import Audio
-        
         Audio('welcome.mp3')
+        
+        audio_file = open(‘welcome.mp3’, ‘rb’)
+        audio_bytes = audio_file.read()
+        st.audio(audio_bytes, format=‘audio/ogg’)       
+        
       
 else: pass
