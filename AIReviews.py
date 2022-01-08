6a455be0824417964b0cc6bcb13061baaccc255b
Vikas <47058321+ipvikas@users.noreply.github.com>
2022-01-08 20:32:23 +0530

    Update AIReviews.py

--- AIReviews.py
+++ AIReviews.py
@@ -15,7 +15,7 @@ import streamlit as st
 
 def download_model():
     model_name = 'tuner007/pegasus_paraphrase'
-    #torch_device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    torch_device = 'cuda' if torch.cuda.is_available() else 'cpu'
     tokenizer = PegasusTokenizer.from_pretrained(model_name)
     #model = PegasusForConditionalGeneration.from_pretrained(model_name).to(torch_device)
     model = PegasusForConditionalGeneration.from_pretrained(model_name)
