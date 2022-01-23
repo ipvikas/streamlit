6b2ad1d90862e4a18f29b129ae9a1a3e7aa63036
Vikas <47058321+ipvikas@users.noreply.github.com>
2022-01-24 00:03:08 +0530

    Update AIReviews.py
    CHANGED

--- AIReviews.py
+++ AIReviews.py
@@ -7,9 +7,10 @@
 #! pip install transformers
 #! pip install SentencePiece
 
-import torch
+#import torch
 from transformers import PegasusForConditionalGeneration, PegasusTokenizer
 import streamlit as st
+import pandas as pd
 
 @st.cache(allow_output_mutation=True, suppress_st_warning=True)
 
@@ -18,7 +19,7 @@ def download_model():
     #torch_device = 'cuda' if torch.cuda.is_available() else 'cpu'
     tokenizer = PegasusTokenizer.from_pretrained(model_name)
     #model = PegasusForConditionalGeneration.from_pretrained(model_name).to(torch_device)
-    model = PegasusForConditionalGeneration.from_pretrained(model_name, from_tf=True, force_download=True)
+    model = PegasusForConditionalGeneration.from_pretrained(model_name)
     return model, tokenizer
     
 st.title('AI Reviews')
@@ -35,7 +36,18 @@ if st.button('Click to get AI Reviews'):
         translated = model.generate(**batch,max_length=60,num_beams=10, num_return_sequences=10, temperature=1.5)
         tgt_text = tokenizer.batch_decode(translated, skip_special_tokens=True)
         st.write('', str(tgt_text))#for streamlit
-else: pass
         
+        contentDF = tgt_text
+        dataframeFinal = pd.DataFrame(contentDF)
+        csv = dataframeFinal.to_csv(index=True)
+
+        st.download_button(label="Download CSV", data=csv,mime="text/csv",file_name="AIReviews.csv")
+        
+    if st.button('Clear output'):
+        st.text_area.empty()
+#else: pass
+        
+else: pass
+   
 num_return_sequences = 10
 num_beams = 10
